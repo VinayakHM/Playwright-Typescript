a9cb6404b1d606299ec932ef1a955a2d3d6e4d5b
vinayak <vinayakhk9@gmail.com>
2025-04-23 17:02:54 +0530

    fixed tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,6 +7,27 @@ test.skip('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+test('has title 1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Vinayak/);
+});
+
+test('has title 2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Vinayak/);
+});
+
+test('has title 3', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Vinayak/);
+});
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
