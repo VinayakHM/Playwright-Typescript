ac00e22d750275741f1a3b80c76fe04c6763b8c6
Vinayak <35108182+VinayakHM@users.noreply.github.com>
2025-05-14 13:26:41 +0530

    Update Login.spec.ts
    code fix

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Page } from '@playwright/test';
 
 test('Example test 1',async()=>{
   console.log('Print the env file fields : '+process.env.user);
-  console.log('Print the env file fields password : '+process.env.pwd');
+  console.log('Print the env file fields password : '+process.env.pwd);
   console.log('Hello');
   await expect(process.env.user).toEqual('vinayak');
 })
