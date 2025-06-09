f723332a0ca06b5b38b005d0490421c81b2e3dff
Vinayak M <vinayakm@Vinayaks-MacBook-Pro.local>
2025-06-09 23:51:12 +0530

    test

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -5,3 +5,8 @@ test('Example test 1',async()=>{
   console.log('Print the env file fields password : '+process.env.pwd);
   console.log('Hello');
 })
+
+test('Automate Myntra Web App',async({page})=>{
+  await page.goto('https://www.flipkart.com/');
+  await expect(page).toHaveTitle(/Flipkart/);
+  })
